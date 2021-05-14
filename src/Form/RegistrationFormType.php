bc6bd9f4c21865bf17bfc9139d58aae421f56b9e
fra9106 <contact@monpersoweb.fr>
2021-05-14 12:37:56 +0200

    add Funtion getConfigurationForm in RegistrationFormType, edit navbar and css

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,41 +18,29 @@ use Symfony\Component\Form\Extension\Core\Type\UrlType;
 
 class RegistrationFormType extends AbstractType
 {
+
+    protected function getConfigurationForm($label, $placeholder, $options = []) {
+        return array_merge_recursive([
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder,
+                'required' => false
+            ]
+        ], $options);
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('first_name', TextType::class, [
-                'attr' => ['placeholder' => 'Votre nom'],
-                'required' => false,
-            ])
-            ->add('last_name', TextType::class, [
-                'attr' => ['placeholder' => 'Votre prénom'],
-                'required' => false,
-            ])
-            ->add('email', EmailType::class, [
-                'attr' => ['placeholder' => 'Votre adresse mail'],
-                'required' => false,
-            ])
-            ->add('password', PasswordType::class, [
-                'attr' => ['placeholder' => 'Choisissez un mot de passe'],
-                'required' => false,
-            ])
-            ->add('confirm_password', PasswordType::class, [
-                'attr' => ['placeholder' => 'Confirmez votre mot de passe'],
-                'required' => false,
-            ])
-            ->add('picture', UrlType::class, [
-                'attr' => ['placeholder' => 'Choisisez un avatar'],
-                'required' => false,
-            ]) 
-            ->add('introduction', TextType::class, [
-                'attr' => ['placeholder' => 'Choisisez une phrase qui vous représente le mieux'],
-                'required' => false,
-            ])
-            ->add('description', TextareaType::class, [
-                'attr' => ['placeholder' => 'Une petite description de votre personalité'],
-                'required' => false,
-            ])
+            ->add('first_name', TextType::class,  $this->getConfigurationForm('Nom','Votre nom'))
+            ->add('last_name', TextType::class,  $this->getConfigurationForm('prénom','Votre prénom'))
+            ->add('email',  EmailType::class,  $this->getConfigurationForm('Email','Votre email'))
+            ->add('password', PasswordType::class,  $this->getConfigurationForm('Mot de passe','Choisissez un mot de passe'))
+            ->add('confirm_password', PasswordType::class,  $this->getConfigurationForm('Confimation de mot de passe','Confirmez votre mot de passe'))
+            ->add('picture', UrlType::class,  $this->getConfigurationForm('Avatar','Choisisez un avatar'))
+            ->add('introduction', TextType::class,  $this->getConfigurationForm('Into','Choisisez une phrase qui vous représente le mieux'))
+            ->add('description', TextareaType::class,  $this->getConfigurationForm('Description','Une petite description de votre personalité'))
+            
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
