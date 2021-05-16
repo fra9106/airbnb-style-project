290a91c6cf50a7a38e1c335e4b88ecc4ed22b5d0
fra9106 <contact@monpersoweb.fr>
2021-05-16 12:54:29 +0200

    create accountUser feature

---
+++ src/Controller/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/account-user/", name="app_account_user")
+     * 
+     * @IsGranted("ROLE_USER")
+     * 
+     * @return Response
+     */
+    public function accountUser(): Response
+    {
+        return $this->render('user/profile_display.html.twig', [
+            'user' => $this->getUser()
+        ]);
+    }
+}
