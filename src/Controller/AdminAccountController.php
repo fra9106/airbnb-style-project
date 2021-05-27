20defd7eec0a43bfe4f95b59268dc97ef8465dc5
fra9106 <contact@monpersoweb.fr>
2021-05-27 13:50:39 +0200

    create login admin LoginFormAuthenticatorAdmin

---
+++ src/Controller/AdminAccountController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminAccountController extends AbstractController
+{
+    /**
+     * @Route("/admin/login", name="app_admin_account_login")
+     */
+    public function adminLogin(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('admin/account/admin_login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+}
