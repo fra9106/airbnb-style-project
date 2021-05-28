665139eb90fb5d330abad2e2e167dc1990a5820b
fra9106 <contact@monpersoweb.fr>
2021-05-28 18:52:29 +0200

    edit comment admin

--- src/Controller/AdminCommentController.php
+++ src/Controller/AdminCommentController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\AdminCommentEditType;
 use App\Repository\CommentRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminCommentController extends AbstractController
 {
@@ -17,5 +21,33 @@ class AdminCommentController extends AbstractController
         return $this->render('admin/comment/admin_comments_list.html.twig', [
             'comments' => $comments->findAll()
         ]);
+    }
+
+    /**
+     *@Route("/admin/comment/{id}/edit", name="admin_comment_edit")
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param Comment $comment
+     * @return Response
+     */
+    public function adminCommentEdit(Request $request, EntityManagerInterface $manager, Comment $comment):Response
+    {
+        $form = $this->createForm(AdminCommentEditType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash('success', "le commentaire numéro : {$comment->getId()} a bien été modififié 🤗");
+        }
+
+        return $this->render('admin/comment/admin_comment_edit.html.twig', [
+            'form' => $form->createView(),
+            'comment' => $comment
+        ]);
+
     }
 }
